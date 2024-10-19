45efeb05c2afcc56c60423446e7b752a4491178a
Rizki Abdul Gofur <rizky.abdulghofur@gmail.com>
2024-10-19 14:48:57 +0700

    add doc on main.dart and change readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# flutter_provider_example
+# Flutter Provider Example
 
 Learning Flutter State Management using Provider, this is sample of code. So, you may know how it works.
--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,26 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Use the MultiProvider widget to create a provider tree that can be
+    // accessed by all widgets in the app.
+    //
+    // The ChangeNotifierProvider widget is used to create a provider that
+    // wraps a ChangeNotifier (our CounterProvider class). This provider
+    // will automatically call notifyListeners() whenever the notifyListeners()
+    // method is called on the wrapped ChangeNotifier.
+    //
+    // The create callback is used to create an instance of the
+    // ChangeNotifier. In this case, we create an instance of
+    // CounterProvider with an initial value of 0.
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (_) => CounterProvider(number: 0),
         ),
       ],
+      // The child widget is the top level widget of the app.
+      // In this case, we use the MaterialApp widget, with the home
+      // property set to the HomePage widget.
       child: const MaterialApp(
         home: HomePage(),
       ),
